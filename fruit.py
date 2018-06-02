15910ce282b3b661762bde77f469bc1577d66d95
dugulianxian <xukknonglec@163.com>
2018-06-02 15:05:49 +0800

    ff

--- fruit.py
+++ fruit.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import numpy as np
-from pandas import DataFrame
 import matplotlib.pyplot as plt
-from numpy.random import randn
-from pandas import Series
+import pandas as pd
 
 # filename可以直接从盘符开始，标明每一级的文件夹直到csv文件，header=None表示头部为空，sep=' '表示数据间使用空格作为分隔符，如果分隔符是逗号，只需换成 ‘，’即可。
 a = pd.read_csv('fruitveg.csv', sep=',')
